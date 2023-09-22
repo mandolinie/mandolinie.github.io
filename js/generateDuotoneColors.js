13c3e5ba24e78510fa55e3175a5ae5ed406c5db5
Manuel Blex <38181916+mandolinie@users.noreply.github.com>
2023-09-22 17:21:08 +0200

    Duotone script changes
    - added script ID in head
    - <style> element is added before CSS

--- js/generateDuotoneColors.js
+++ js/generateDuotoneColors.js
@@ -67,69 +67,12 @@ document.addEventListener('DOMContentLoaded', function () {
     themeColorMetaTag.setAttribute('content', color1);
   }
 
-  document.head.appendChild(styleElement);
+  // Find the script with the ID 'custom-script'
+  const scriptPosition = document.getElementById('duotone');
+
+  // Insert the style element after the custom script
+  if (scriptPosition) {
+    scriptPosition.insertAdjacentElement('afterend', styleElement);
+  }
 
 });
-
-/*
-document.addEventListener('DOMContentLoaded', function () {
-
-  function generateDuotoneColors(contrastRatio) {
-    let color1, color2;
-    do {
-      color1 = getRandomColor();
-      color2 = getRandomColor();
-    } while (getContrastRatio(color1, color2) < contrastRatio);
-
-    return [color1, color2];
-  }
-
-  function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  function getContrastRatio(color1, color2) {
-    const luminance1 = getLuminance(color1);
-    const luminance2 = getLuminance(color2);
-
-    const brightest = Math.max(luminance1, luminance2);
-    const darkest = Math.min(luminance1, luminance2);
-
-    return (brightest + 0.05) / (darkest + 0.05);
-  }
-
-  function getLuminance(color) {
-    if (color.charAt(0) === '#') {
-      color = color.substr(1);
-    }
-    if (color.length === 3) {
-      color = color.split('').map((char) => char + char).join('');
-    }
-    const r = parseInt(color.substr(0, 2), 16);
-    const g = parseInt(color.substr(2, 2), 16);
-    const b = parseInt(color.substr(4, 2), 16);
-    return 0.2126 * r + 0.7152 * g + 0.0722 * b;
-  }
-
-  // Specify your contrast ratio (e.g., 4.5:1 for WCAG AA)
-  const contrastRatio = 4.5;
-
-  // Call the function to generate duotone colors with the specified contrast ratio
-  const [color1, color2] = generateDuotoneColors(contrastRatio);
-
-  // Inject the color variables directly into the head of the document as a <style> element
-  const styleElement = document.createElement('style');
-  styleElement.innerHTML = `:root {
-    --primary: ${color1};
-    --secondary: ${color2};
-  }`;
-
-  document.head.appendChild(styleElement);
-
-});
-*/
